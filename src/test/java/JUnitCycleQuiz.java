23f771c44eb5746e50458f44fe083a32231eca9c
philocsera <uingdeavour02@gmail.com>
2024-07-20 15:38:28 +0900

    Test: Add JUnitCycleQuiz

--- src/test/java/JUnitCycleQuiz.java
+++ src/test/java/JUnitCycleQuiz.java
@@ -1,2 +1,25 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class JUnitCycleQuiz {
+    @BeforeEach
+    public void sayHello(){
+        System.out.println("Hello!");
+    }
+    
+    @AfterAll
+    static void sayGoodBye(){
+        System.out.println("Bye!");
+    }
+
+    @Test
+    public void Quiz3(){
+        System.out.println("First Test");
+    }
+
+    @Test
+    public void Quiz4(){
+        System.out.println("Second Test");
+    }
 }
